a34d421a41f9a966bb627b6322e91ff25995aae7
Dragon-L <530593201@qq.com>
2018-11-12 19:29:19 +0800

    create basic restful web service

---
+++ src/main/java/l/backenddemo/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package l.backenddemo.Controller;
+
+import l.backenddemo.Utils.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    private final String template = "Hello, %s!";
+
+    @RequestMapping("/hello")
+    public Greeting Hello(@RequestParam(value = "name", defaultValue = "World")String name){
+        return new Greeting(String.format(template, name));
+    }
+}
---
+++ src/main/java/l/backenddemo/Utils/Greeting.java
@@ -0,0 +1,13 @@
+package l.backenddemo.Utils;
+
+public class Greeting {
+    private final String Content;
+
+    public Greeting(String content) {
+        Content = content;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+}
